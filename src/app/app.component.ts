93650b7fbb3b95e1587b287305c37968a2265765
Ramses Feliciano <ramsesfeliciano@gmail.com>
2024-06-12 15:23:16 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from './models/menu-item';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,49 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'PizzaLab';
+  Menu:MenuItem[] = [
+    {
+      name: "Chicken Fingers",
+      category: "Dinner",
+      price: 11.99,
+      vegetarian:false,
+      soldOut:false
+    },
+    {
+      name: "Pizza",
+      category: "Dinner",
+      price: 11.99,
+      vegetarian:true,
+      soldOut:false
+    },
+    {
+      name: "Pepperoni Pizza",
+      category: "Dinner",
+      price: 12.99,
+      vegetarian:false,
+      soldOut:false
+    },
+    {
+      name: "Bread Sticks",
+      category: "Sides",
+      price: 4.99,
+      vegetarian:true,
+      soldOut:true
+    },
+    {
+      name: "Wings",
+      category: "Sides",
+      price: 8.99,
+      vegetarian:false,
+      soldOut:false
+    },
+    {
+      name: "Cinnamon Roll",
+      category: "Dessert",
+      price: 8.99,
+      vegetarian:true,
+      soldOut:false
+    },
+  ]
 }
+
